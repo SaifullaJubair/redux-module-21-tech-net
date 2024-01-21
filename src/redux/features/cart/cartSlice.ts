fa5b40dedcb332c29d4a9b80d4179a42bc28cdf4
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-21 16:11:17 +0600

    cart increment decrement delete logic implement

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -23,9 +23,30 @@ const cartSlice = createSlice({
         state.products.push({ ...action.payload, quantity: 1 });
       }
     },
+
+    removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
+      const existingProducts = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (existingProducts && existingProducts.quantity! > 1) {
+        existingProducts.quantity = existingProducts.quantity! - 1;
+      } else {
+        state.products = state.products.filter(
+          (product) => product._id != action.payload._id
+        );
+      }
+    },
+
+    removeFromCart: (state, action: PayloadAction<IProduct>) => {
+      state.products = state.products.filter(
+        (product) => product._id != action.payload._id
+      );
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeOneFromCart, removeFromCart } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
